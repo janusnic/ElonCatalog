840d258ba92467e9583bc24e4a761c863ab14fec
Denis Dolgopolov <dolgopolovdc@gmail.com>
2015-04-24 23:50:33 +0400

    add plagin

---
+++ Plugin.php
@@ -0,0 +1,28 @@
+<?php namespace Elon\ElonCatalog;
+
+use System\Classes\PluginBase;
+
+/**
+ * Elon Catalog Plugin Information File
+ * @author Denis Dolgopolov <dolgopolovdc@gmail.com>
+ *
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Elon Catalog',
+            'description' => 'Product catalog.',
+            'author'      => 'Trofim Jugastrov, Dolgopolov Denis, Alexander Khramov, Natalia Kosman',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+}
